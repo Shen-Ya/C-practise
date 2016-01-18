0ebdf68d0ab8ae149cf661f70dd88edca2adf88d
Shen-Ya <707475639@qq.com>
2016-01-18 10:00:34 +0800

    16:10
    十六进制转化成十进制

---
+++ 进制转换2.c
@@ -0,0 +1,22 @@
+从键盘上输入一个不超过8的正的十六进制数字符串，将它转为正的十进制数后输出。
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+int main()
+{
+	double sum = 0, x;
+	char a[8];
+	int len, i = 0;
+	gets(a);
+	len = strlen(a);
+	while(len)
+	{
+		if(a[len - 1] >= 'A' && a[len - 1] <= 'F')
+			x = (a[len - 1] - '7') *pow (16,i++);
+		sum + = x;
+		len--;
+	}
+	printf("%.01f",sum);
+	return 0;
+}
